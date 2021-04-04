df947bfbecd63b987576cd6d7720b4acd7cf2c8b
healthyCoder <36478181+healthyCoder@users.noreply.github.com>
2021-04-04 11:00:26 +0530

    Completed sending and storing images

--- cynosure/App.py
+++ cynosure/App.py
@@ -1,11 +1,8 @@
 #!/usr/bin/env python
 
-# WS server that sends messages at random intervals
-
 import asyncio
-import datetime
-import random
 import websockets
+import base64
 
 wsHost = "127.0.0.1"
 wsPort = 5678
@@ -23,7 +20,20 @@ async def pingPong(websocket, path):
       await websocket.send(errMsg)
       print(errMsg)
 
-start_server = websockets.serve(pingPong, wsHost, wsPort)
+
+async def echoReceived(websocket, path):
+  async for message in websocket:
+    print("Received data!")
+    startLoc = message.find("64,")+3
+    imageData = message[startLoc:]
+
+    if message != "PING":
+      with open("imageReceived.png", "wb") as fh:
+        fh.write(base64.b64decode(imageData))
+        print("Saved Image")
+    await websocket.send("Gotcha")
+
+start_server = websockets.serve(echoReceived, port = wsPort)
 asyncio.get_event_loop().run_until_complete(start_server)
 print("Started Server and waiting for clients")
 asyncio.get_event_loop().run_forever()
